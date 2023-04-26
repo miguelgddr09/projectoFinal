c9f1dc7b569c47d85aed655d2673a135814eece3
miguelgddr09 <122293245+miguelgddr09@users.noreply.github.com>
2023-04-26 10:33:38 -0600

    Update Canvas.java

--- Canvas.java
+++ Canvas.java
@@ -10,6 +10,7 @@ public class Canvas extends JPanel  {
 	
 	static BufferedImage imagen;
 	static Graphics gfx;
+	int pixelSize=40;
 	
 	public Canvas() {
 		setBackground(Color.black);
@@ -17,6 +18,14 @@ public class Canvas extends JPanel  {
 		gfx=imagen.createGraphics();
 	}
 	
+	public void setGridSize(int size, Color color) {
+		for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {                
+                gfx.setColor(color);
+                gfx.drawRect(i * pixelSize, j * pixelSize, pixelSize, pixelSize);
+            }
+        }
+	}
 	public void setImage(BufferedImage imgagenSet) {
 		imagen=imgagenSet;
 	}
