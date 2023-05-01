1c0148bf1ffdbccebaa939604793a5fa161340e4
dieGod194 <122294998+dieGod194@users.noreply.github.com>
2023-05-01 11:44:08 -0600

    Update Canvas.java

--- Canvas.java
+++ Canvas.java
@@ -1,68 +1,74 @@
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JPanel;
 
-public class Canvas extends JPanel  {
+public class Canvas extends JPanel {
 	
-	static BufferedImage imagen;
-	static Graphics gfx;
-	int pixelSize=40;
-	private final int TAMANIO_CASILLA = 10;
-   	private final int FILAS = 50;
-    	private final int COLUMNAS = 50;
-    	private Color[][] casillas = new Color[FILAS][COLUMNAS];
+	private static BufferedImage imagen;
+	private static Graphics gfx;
+	private final int PIXEL_SIZE = 40;
+	private final int ROWS = 50;
+	private final int COLUMNS = 50;
+	private Color[][] casillas = new Color[ROWS][COLUMNS];
 	
 	public Canvas() {
-		setBackground(Color.black);
-		imagen = new BufferedImage(500, 500, BufferedImage.TYPE_INT_RGB);
-		gfx=imagen.createGraphics();
+		setBackground(Color.BLACK);
+		imagen = new BufferedImage(PIXEL_SIZE * COLUMNS, PIXEL_SIZE * ROWS, BufferedImage.TYPE_INT_RGB);
+		gfx = imagen.createGraphics();
+		resetCanvas();
 	}
 	
-	private void paintPixel(int x, int y, Color color) {
+	private void resetCanvas() {
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				casillas[i][j] = Color.WHITE;
+			}
+		}
+		repaint();
+	}
+	
+	public void paintPixel(int row, int col, Color color) {
 		gfx.setColor(color);
-		gfx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
+		gfx.fillRect(col * PIXEL_SIZE, row * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+		repaint();
 	}
 	
-	  public Cuadricula() {
-        for (int fila = 0; fila < FILAS; fila++) {
-            for (int columna = 0; columna < COLUMNAS; columna++) {
-                casillas[fila][columna] = Color.WHITE;
-            }
-        }
-    }
-
-    public void setColor(int fila, int columna, Color color) {
-        casillas[fila][columna] = color;
-        repaint();
-    }
-	
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        for (int fila = 0; fila < FILAS; fila++) {
-            for (int columna = 0; columna < COLUMNAS; columna++) {
-                g.setColor(casillas[fila][columna]);
-                g.fillRect(columna * TAMANIO_CASILLA, fila * TAMANIO_CASILLA, TAMANIO_CASILLA, TAMANIO_CASILLA);
-                g.setColor(Color.BLACK);
-                g.drawRect(columna * TAMANIO_CASILLA, fila * TAMANIO_CASILLA, TAMANIO_CASILLA, TAMANIO_CASILLA);
-            }
-        }
-    }
-
-    @Override
-    public int getPreferredSize() {
-        return new Dimension(COLUMNAS * TAMANIO_CASILLA, FILAS * TAMANIO_CASILLA);
-    }
-}
-	
-	public void setImage(BufferedImage imgagenSet) {
-		imagen=imgagenSet;
+	public void setColor(int row, int col, Color color) {
+		casillas[row][col] = color;
+		repaint();
 	}
 	
-	public Graphics getGraphic() {
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		g.drawImage(imagen, 0, 0, null);
+		for (int i = 0; i < ROWS; i++) {
+			for (int j = 0; j < COLUMNS; j++) {
+				g.setColor(Color.BLACK);
+				g.drawRect(j * PIXEL_SIZE, i * PIXEL_SIZE, PIXEL_SIZE, PIXEL_SIZE);
+			}
+		}
+	}
+	
+	@Override
+	public Dimension getPreferredSize() {
+		return new Dimension(COLUMNS * PIXEL_SIZE, ROWS * PIXEL_SIZE);
+	}
+	
+	public void setImage(BufferedImage imagenSet) {
+		imagen = imagenSet;
+		gfx = imagen.createGraphics();
+		resetCanvas();
+	}
+	
+	public Graphics getGfx() {
 		return gfx;
+	}
+	
+	public BufferedImage getImagen() {
+		return imagen;
 	}
 	
 }
