94f48cbc87404a51c536ec5d190d567083fdc26f
dieGod194 <122294998+dieGod194@users.noreply.github.com>
2023-05-01 11:19:26 -0600

    Update Canvas.java

--- Canvas.java
+++ Canvas.java
@@ -9,6 +9,10 @@ public class Canvas extends JPanel  {
 	static BufferedImage imagen;
 	static Graphics gfx;
 	int pixelSize=40;
+	private final int TAMANIO_CASILLA = 10;
+   	private final int FILAS = 50;
+    	private final int COLUMNAS = 50;
+    	private Color[][] casillas = new Color[FILAS][COLUMNAS];
 	
 	public Canvas() {
 		setBackground(Color.black);
@@ -21,14 +25,38 @@ public class Canvas extends JPanel  {
 		gfx.fillRect(x * pixelSize, y * pixelSize, pixelSize, pixelSize);
 	}
 	
-	private void setGridSize(int size, Color color) {
-		for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {                
-                gfx.setColor(color);
-                gfx.drawRect(i * pixelSize, j * pixelSize, pixelSize, pixelSize);
+	  public Cuadricula() {
+        for (int fila = 0; fila < FILAS; fila++) {
+            for (int columna = 0; columna < COLUMNAS; columna++) {
+                casillas[fila][columna] = Color.WHITE;
             }
         }
-	}
+    }
+
+    public void setColor(int fila, int columna, Color color) {
+        casillas[fila][columna] = color;
+        repaint();
+    }
+	
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        for (int fila = 0; fila < FILAS; fila++) {
+            for (int columna = 0; columna < COLUMNAS; columna++) {
+                g.setColor(casillas[fila][columna]);
+                g.fillRect(columna * TAMANIO_CASILLA, fila * TAMANIO_CASILLA, TAMANIO_CASILLA, TAMANIO_CASILLA);
+                g.setColor(Color.BLACK);
+                g.drawRect(columna * TAMANIO_CASILLA, fila * TAMANIO_CASILLA, TAMANIO_CASILLA, TAMANIO_CASILLA);
+            }
+        }
+    }
+
+    @Override
+    public int getPreferredSize() {
+        return new Dimension(COLUMNAS * TAMANIO_CASILLA, FILAS * TAMANIO_CASILLA);
+    }
+}
+	
 	public void setImage(BufferedImage imgagenSet) {
 		imagen=imgagenSet;
 	}
