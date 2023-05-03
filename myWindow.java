5c1077304529533f068deb7ea0a9acdf9efcd7d7
Emilio <122295142+Ezavalam@users.noreply.github.com>
2023-05-03 09:18:11 -0600

    Update myWindow.java

--- myWindow.java
+++ myWindow.java
@@ -3,6 +3,11 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import java.awt.*;
 
+import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
+
 public class myWindow extends JFrame {
 
     public myWindow() {
@@ -10,68 +15,9 @@ public class myWindow extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(500, 800);
 
-        // Panel de colores
-        Colores coloresPanel = new Colores();
-
-        // Panel de colores predefinidos
-        JPanel predefinedColorsPanel = new JPanel() {
-            @Override
-            public void paintComponent(Graphics g) {
-                super.paintComponent(g);
-
-                // blanco
-                g.setColor(Color.WHITE);
-                g.fillRect(20, 140, 40, 40);
-
-                // negro
-                g.setColor(Color.BLACK);
-                g.fillRect(80, 140, 40, 40);
-
-                // azul
-                g.setColor(Color.BLUE);
-                g.fillRect(140, 140, 40, 40);
-
-                // rojo
-                g.setColor(Color.RED);
-                g.fillRect(20, 190, 40, 40);
-
-                // verde
-                g.setColor(Color.GREEN);
-                g.fillRect(80, 190, 40, 40);
-
-                // amarillo
-                g.setColor(Color.YELLOW);
-                g.fillRect(140, 190, 40, 40);
-
-                // cuadro de color actual
-                g.setColor(new Color(coloresPanel.redSlider.getValue(), coloresPanel.greenSlider.getValue(), coloresPanel.blueSlider.getValue()));
-                g.fillRect(200, 140, 30, 90);
-            }
-        };
-        predefinedColorsPanel.setPreferredSize(new Dimension(260, 250));
-
-        // Agrega los paneles al frame principal
-        JPanel mainPanel = new JPanel(new BorderLayout());
-        mainPanel.add(coloresPanel, BorderLayout.WEST);
-        mainPanel.add(predefinedColorsPanel, BorderLayout.CENTER);
-        setContentPane(mainPanel);
-
-        setVisible(true);
-    }
-    
-   // public void mouseClicked(MouseEvent e) {
-		
-		//x = e.getX(); //get the x and y coordinates of
-		//y = e.getY();
-	
-		//canvas.getGraphic().setColor(Color.____); //declarar color que tenga el usuario en ese momento
-		//canvas.getGraphic().fillRect(x-10, y-10, 20, 20);
-		//canvas.getGraphic().setColor(Color.black);
-		//canvas.getGraphic().drawRect(x-10, y-10, 20, 20);
-		//canvas.setImage(img);
-		
-		//lbl.setText(x+" "+y);
-//	}
+        Colores colores = new Colores();
+        colores.funcionesColores();
+}
 
     public static void main(String[] args) {
         new myWindow();
