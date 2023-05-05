1607b71cc682aed5e29fdbf0dc0bb7c4aabbdba0
miguelgddr09 <122293245+miguelgddr09@users.noreply.github.com>
2023-05-05 10:16:15 -0600

    Add files via upload

---
+++ Layout.java
@@ -0,0 +1,30 @@
+package layoutProyect;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Layout {
+	public static void main(String[] args) {
+		/*JFrame frame = new JFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(800,500);
+		frame.setLayout(new BorderLayout());
+		frame.setVisible(true);
+		
+		JPanel panel1 = new JPanel();
+		JPanel panel2 = new JPanel();
+		JPanel panel3 = new JPanel();
+		
+		panel1.setBackground(Color.red);
+		panel2.setBackground(Color.green);
+		panel3.setBackground(Color.blue);
+		
+		frame.add(panel1,BorderLayout.NORTH);
+		frame.add(panel2,BorderLayout.CENTER);
+		frame.add(panel3,BorderLayout.SOUTH);*/
+		
+	}
+}
